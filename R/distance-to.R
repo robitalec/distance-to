8c80765598baafd1b0ff25203f9573f1cd6213ad
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-24 15:43:32 -0700

    set up with example nc data

--- R/distance-to.R
+++ R/distance-to.R
@@ -1,1 +1,14 @@
 library(nabor)
+# install.packages('spData')
+library(sf)
+
+nc <- st_read(system.file("shapes/sids.shp", package="spData")[1], quiet=TRUE)
+
+
+somenc <- nc[sample(length(nc), 10),]
+
+pts <- st_sample(nc, 100)
+
+plot(pts)
+
+
