a228b82d43490a6ca527340a63d46b4578eed497
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-24 16:57:27 -0700

    rm junk

--- R/distance-to.R
+++ R/distance-to.R
@@ -1,18 +1,3 @@
-library(nabor)
-# install.packages('spData')
-library(sf)
-
-nc <- st_read(system.file("shapes/sids.shp", package="spData")[1], quiet=TRUE)
-
-
-somenc <- nc[sample(length(nc), 10),]
-
-pts <- st_sample(nc, 100)
-
-plot(pts)
-
-# check if st_crs is same
-
 #' Distance to
 #'
 #' @param x
@@ -40,15 +25,3 @@ distance_to <- function(x, y) {
 		# dists
 	}
 }
-z <- knn(st_coordinates(pts),
-				 st_coordinates(somenc),
-				 1)
-
-# NOTE:
-# The underlying libnabo does not have a signalling value to identify
-# indices for invalid query points (e.g. those containing an NA).
-# In this situation, the index returned by libnabo will be 0 and
-# knn will therefore return an index of 1. However the distance
-# will be Inf signalling a failure to find a nearest neighbour.
-
-# this is good for linear or pts
