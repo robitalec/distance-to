1dc4053b331a67a151556a44db8b0953bd854915
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-25 11:31:28 -0700

    split out coordinates

--- R/distance-to.R
+++ R/distance-to.R
@@ -10,31 +10,33 @@
 #'
 #' @examples
 distance_to <- function(x, y) {
-	if (length(unique(st_geometry_type(y))) != 1) {
+	# check if x is always pts
+
+	if (length(unique(sf::st_geometry_type(y))) != 1) {
 		stop('sf::st_geometry_type(y) shows mixed geometry types')
 	}
 
 	if (sf::st_crs(x) == sf::st_crs(y)) {
-		stop('st_crs(x) must be the same as st_crs(y)')
+		stop('sf::st_crs(x) must be the same as sf::st_crs(y)')
 	}
-
-	dists <- nabor::knn(data = sf::st_coordinates(y)[, c(1, 2)],
-											query = sf::st_coordinates(x)[, c(1, 2)],
-											k = 1L)$nn.dists
 
 	if (sf::st_is_longlat(x) != sf::st_is_longlat(y)) {
 		stop('both x and y must be long lat degrees, or neither')
-	} else if (sf::st_is_longlat(x) & sf::st_is_longlat(y)) {
-		# recalculate dist with geodist
-		# optional geodist dependency
+	}
+
+	xcoor <- sf::st_coordinates(x)[, c(1, 2)]
+	ycoor <- sf::st_coordinates(y)[, c(1, 2)]
+	dists <- nabor::knn(data = ycoor, query = xcoor, k = 1L)
+
+	if (sf::st_is_longlat(x) & sf::st_is_longlat(y)) {
 	}
 
 	if (sf::st_geometry_type(y, FALSE) %in%
 			c('POINT', 'MULTIPOINT', 'LINESTRING', 'MULTILINESTRING')) {
-		dists
+		dists[['nn.dists']]
 	} else if (sf::st_geometry_type(y, FALSE) %in%
 						 c('POLYGON', 'MULTIPOLYGON')){
-		dists[lengths(st_intersects(x, y)) > 0] <- 0
-		dists
+		dists[['nn.dists']][lengths(st_intersects(x, y)) > 0] <- 0
+		dists[['nn.dists']]
 	}
 }
