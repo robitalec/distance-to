acc429e57b6da6dc3be5b4d1109914032dcd1f9e
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-25 12:44:58 -0700

    add optional extent and expand arguments

--- R/distance-raster.R
+++ R/distance-raster.R
@@ -2,19 +2,37 @@
 #'
 #' Warnings + recommend pt based
 #'
-#' @param x
 #' @param cellsize in unit of projection
+#' @param y
+#' @param extent optional alternative extent bbox
+#' @param expand 0-1 scaling eg. 5% expansion = 0.05
 #'
 #' @return
 #' @export
 #'
 #' @examples
-distance_raster <- function(x, y, cellsize) {
+distance_raster <- function(y, cellsize, extent = NULL, expand = NULL) {
 	# check cellsize ~~ crs units
 
-	g <- sf::st_make_grid(x, cellsize = cellsize, what = 'centers')
+	if (!is.null(extent) & is.null(expand)) {
+		# check if extent is bbox
+		x <- extent
+	} else if (is.null(extent) & !is.null(expand)) {
+		x <- st_bbox(y) - (st_bbox(y) * rep(c(-expand, expand), 2))
+	} else if (is.null(extent) & is.null(expand)) {
+		x <- y
+	} else if (!is.null(extent) & !is.null(expand)) {
+		# check if extent is bbox
+		x <- extent - (extent * rep(c(-expand, expand), 2))
+	}
+
+	g <- sf::st_make_grid(
+		x,
+		cellsize = cellsize,
+		what = 'centers'
+	)
 
 	g$dist <- distance_to(g, y)
 
-	fasterize::fasterize(g, raster(g), field = 'dist')
+	fasterize::fasterize(g, fasterize::raster(g), field = 'dist')
 }
