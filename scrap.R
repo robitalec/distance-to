66f402f585a50c45bc3af65916ee782ef4242b5f
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-25 11:15:32 -0700

    sample bboxes

--- scrap.R
+++ scrap.R
@@ -5,27 +5,28 @@ library(nabor)
 # install.packages('spData')
 library(sf)
 
-npts <- 1e5
+npts <- 1e4
 
 sample_bbox <- function(shape, n) {
 	bbox <- st_bbox(shape)
 	st_as_sf(data.frame(
 		x = runif(n, bbox[1], bbox[3]),
 		y = runif(n, bbox[2], bbox[4])
-	), coords = c('x', 'y'))
+	), coords = c('x', 'y'),
+	crs = st_crs(shape))
 }
 
 
 nc <- st_read(system.file("shapes/sids.shp", package="spData"))
+st_crs(nc) <- "+proj=longlat +datum=NAD27"
 somenc <- nc[sample(length(nc), 5),]
+somencpts <- sample_bbox(somenc, npts)
 
-somencpts <- st_as_sf(data.table(
-	npts)
 
-seine
+data('seine', package = 'spData')
 bufseine <- st_buffer(seine, 1e3)
-seinepts <- st_sample(bufseine, npts)
-seineotherpts <- st_sample(bufseine[2, ], npts)
+seinepts <- sample_bbox(bufseine[1, ], npts)
+seineotherpts <- sample_bbox(bufseine[2, ], npts)
 
 # Source ------------------------------------------------------------------
 source('R/distance-to.R')
