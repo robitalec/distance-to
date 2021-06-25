cf60aa47cc3c300711e30dbb77ca9d885ea9539f
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-25 12:30:26 -0700

    make grid and fasterize it

--- R/distance-raster.R
+++ R/distance-raster.R
@@ -1,12 +1,18 @@
 #' Distance raster
 #'
+#' Warnings + recommend pt based
+#'
 #' @param x
-#' @param cellsize
+#' @param cellsize in unit of projection
 #'
 #' @return
 #' @export
 #'
 #' @examples
-distance_raster <- function(x, cellsize) {
+distance_raster <- function(x, y, cellsize) {
+	g <- sf::st_make_grid(x, cellsize = cellsize, what = 'centers')
 
+	g$dist <- distance_to(g, y)
+
+	fasterize::fasterize(, raster(g))
 }
