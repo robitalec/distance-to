de2af40b612ee6d8005e3f754c9fd8df51193a43
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-25 11:51:08 -0700

    setup test

--- inst/tinytest/test_distanceto.R
+++ inst/tinytest/test_distanceto.R
@@ -1,4 +1,22 @@
+library(tinytest)
+library(distanceto)
 
-# Placeholder with simple test
-expect_equal(1 + 1, 2)
+library(spData)
+library(sf)
 
+npts <- 1e4
+
+sample_bbox <- function(shape, n) {
+	bbox <- sf::st_bbox(shape)
+	sf::st_as_sf(data.frame(
+		x = runif(n, bbox[1], bbox[3]),
+		y = runif(n, bbox[2], bbox[4])
+	), coords = c('x', 'y'),
+	crs = sf::st_crs(shape))
+}
+
+
+nc <- st_read(system.file("shapes/sids.shp", package="spData"))
+st_crs(nc) <- "+proj=longlat +datum=NAD27"
+somenc <- nc[sample(length(nc), 5),]
+ncpts <- sample_bbox(nc, npts)
