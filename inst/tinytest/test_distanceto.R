f935312ee7446e6eb40df85c26e47811f0d91371
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-26 11:49:45 -0700

    fst full globe check

--- inst/tinytest/test_distanceto.R
+++ inst/tinytest/test_distanceto.R
@@ -20,3 +20,20 @@ nc <- st_read(system.file("shapes/sids.shp", package="spData"))
 st_crs(nc) <- "+proj=longlat +datum=NAD27"
 somenc <- nc[sample(length(nc), 5),]
 ncpts <- sample_bbox(nc, npts)
+
+
+
+
+# Full globe
+measure <- 'geodesic'
+full <- st_as_sf(expand.grid(lon = seq(-180, 180, 5), lat = seq(-90, 90, 5)),
+								 coords = c('lon', 'lat'), crs = 4326)
+i <- sample(nrow(full), nrow(full) / 2)
+o <- seq.int(nrow(full))[!seq.int(nrow(full)) %in% i]
+d <- distance_to(full[i,], full[o,], measure = measure)
+g <- geodist::geodist(
+	st_coordinates(full[i,]),
+	st_coordinates(full[o,]),
+	measure = measure
+)
+g
