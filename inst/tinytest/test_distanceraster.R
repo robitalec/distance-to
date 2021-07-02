1323dd5219dce3455e2a1f680790b10b41bc61bc
Alec L. Robitaille <robit.alec@gmail.com>
2021-07-01 19:01:08 -0700

    fst test distance_raster

---
+++ inst/tinytest/test_distanceraster.R
@@ -0,0 +1,51 @@
+# === Test distance_raster ------------------------------------------------
+
+
+
+
+# Packages ----------------------------------------------------------------
+library(tinytest)
+library(distanceto)
+
+library(spData)
+library(sf)
+
+
+
+# Data --------------------------------------------------------------------
+nc <- st_read(system.file("shapes/sids.shp", package="spData"))
+st_crs(nc) <- "+proj=longlat +datum=NAD27"
+
+data('seine', package = 'spData')
+
+
+# Run ---------------------------------------------------------------------
+rgeo <- distance_raster(nc)
+rproj <- distance_raster(seine)
+
+
+
+# Tests -------------------------------------------------------------------
+# Output types
+expect_inherits(rgeo, 'raster')
+expect_equal(typeof(rgeo), 'double')
+
+expect_equal(typeof(rgeo), typeof(rproj))
+expect_equal(class(rgeo), class(rproj))
+
+
+# Output limits
+expect_true(all(d >= 0))
+
+expect_equal(all(rgeo >= 0), all(rproj >= 0))
+
+
+# Warnings
+
+# Errors
+# Package "fasterize" needed for distance_raster(). Please install it.
+
+
+# extent must be of class bbox from sf::st_bbox
+
+# cellsize selected may result in long run times
